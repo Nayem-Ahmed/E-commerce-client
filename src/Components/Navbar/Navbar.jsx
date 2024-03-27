c7220880abf1433c990e2d2d3b166a9b24585994
unknown <freelancernayem.me@gmail.comm>
2024-03-27 22:56:41 +0600

    navbar modify

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -60,32 +60,27 @@
 
 
 
-
+// Navbar.js
 import React, { useState } from 'react';
 import { FaBars, FaTimes } from 'react-icons/fa';
 import { Link, NavLink } from 'react-router-dom';
 import './navbar.css';
 import logo from '../../assets/e-logo.png';
-import { IoPersonOutline } from "react-icons/io5";
-import { IoCartOutline } from "react-icons/io5";
+import { IoPersonOutline, IoLogOutOutline, IoCartOutline } from "react-icons/io5";
 import useAuth from '../../API/useAuth';
+import { MdArrowDropDown } from "react-icons/md";
+
 
 const Navbar = () => {
-    const {logOut} = useAuth();
+    const { user, logOut } = useAuth();
     const [isOpen, setIsOpen] = useState(false);
-    const [searchQuery, setSearchQuery] = useState('');
 
     const toggleMenu = () => {
         setIsOpen(!isOpen);
     };
 
     const closeMenu = () => {
-        setIsOpen(false); // Close the menu
-    };
-
-    const handleSearchChange = (e) => {
-        setSearchQuery(e.target.value);
-        // Here you can implement functionality to trigger search based on the entered query
+        setIsOpen(false);
     };
 
     return (
@@ -94,20 +89,6 @@ const Navbar = () => {
                 <div className="navbar-logo">
                     <Link to="/"><img className='md:max-w-44 h-10' src={logo} alt="Logo" /></Link>
                 </div>
-                <div className={`menu-icon ${isOpen ? 'open' : ''}`} onClick={toggleMenu}>
-                    {isOpen ? <FaTimes /> : <FaBars />}
-                </div>
-                {/* Search Input */}
-                <div className="search-container">
-                    <input
-                        type="text"
-                        placeholder="Search products..."
-                        value={searchQuery}
-                        onChange={handleSearchChange}
-                        className="search-input rounded-full"
-                    />
-                    {/* You can add a search button here if needed */}
-                </div>
                 <ul className={`nav-menu ${isOpen ? 'active' : ''}`}>
                     <li><NavLink to="/" onClick={closeMenu}>HOME</NavLink></li>
                     <li><NavLink to="/shop" onClick={closeMenu}>SHOP</NavLink></li>
@@ -115,25 +96,49 @@ const Navbar = () => {
                     <li><NavLink to="/women" onClick={closeMenu}>WOMEN</NavLink></li>
                     <li><NavLink to="/kids" onClick={closeMenu}>KIDS</NavLink></li>
                 </ul>
-                    < Link to="/cart" onClick={closeMenu}>
-                        <div className='flex items-center'>
-                            <IoCartOutline className='text-2xl'>
-                            </IoCartOutline>
-                            <span className='relative -mt-8 bg-[#eb2f06] text-white h-5 w-5 rounded-full text-center leading-[20px]'>0</span>
+                <div className='flex items-center gap-5'>
 
-                        </div>
-                    </Link> 
+                {user?.email ?
+                    <div className="header-menu">
+                        <ul>
+                            <li>
+                                <div className='flex items-center cursor-pointer'>
+                                    <img className='rounded-full w-8 border' src={user?.photoURL} alt="" />
+                                    <span className='text-sm'>Hello, {user?.email && user.email.slice(0, user.email.indexOf('@'))}</span>
+                                    <span><MdArrowDropDown></MdArrowDropDown></span>
+                                </div>
 
-                {/* Login Button */}
-                <Link to='/login' onClick={closeMenu} className="login-link">
-                    <button onClick={logOut} className="font-semibold md:py-2 py-1 px-2 flex items-center gap-1 md:px-5 rounded-3xl border border-black ">
-                        <IoPersonOutline className='md:text-xl'></IoPersonOutline> LOGIN
-                    </button>
+                                <ul className="dropdown-menu md:w-40 shadow-md">
+                                    <li><Link to="#">My Wishlist</Link></li>
+                                    <li><Link to="#">My Oders</Link></li>
+                                    <li><Link to="#">My Reviews</Link></li>
+                                    <li><Link><button onClick={logOut} className='flex items-center gap-1' type='button'><IoLogOutOutline></IoLogOutOutline>Logout</button></Link></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                    :
+                    <Link to='/login' onClick={closeMenu} className="login-link">
+                        <button onClick={logOut} className="font-semibold md:py-2 py-1 px-2 flex items-center gap-1 md:px-5 rounded-3xl border border-black ">
+                            <IoPersonOutline className='md:text-xl' /> LOGIN
+                        </button>
+                    </Link>
+                }
+                <Link to="/cart" onClick={closeMenu}>
+                    <div className='flex items-center'>
+                        <IoCartOutline className='text-2xl' />
+                        <span className='relative -mt-8 bg-[#eb2f06] text-white h-5 w-5 rounded-full text-center leading-[20px]'>0</span>
+                    </div>
                 </Link>
+                <div className={`menu-icon ${isOpen ? 'open' : ''}`} onClick={toggleMenu}>
+                    {isOpen ? <FaTimes /> : <FaBars />}
+                </div>
+                </div>
             </div>
         </nav>
     );
 };
 
 export default Navbar;
+
 
